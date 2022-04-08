830540073569defbbd8842d7af7e96587efe1f94
Miles Whiticker <miles.whiticker@gmail.com>
2022-04-08 16:59:55 +1000

    particles spawn at a random pos by default but can be forced to a position (eg grid)
    Signed-off-by: Miles Whiticker <miles.whiticker@gmail.com>

--- Assets/Scripts/SimController/CreateParticle.cs
+++ Assets/Scripts/SimController/CreateParticle.cs
@@ -6,7 +6,13 @@ using UnityEngine;
 
 public partial class SimController : MonoBehaviour
 {
-    public void CreateParticle()
+    public Particle CreateParticle(Vector3 aForcedPos)
+    {
+        Particle particle = CreateParticle();
+        particle.transform.position = aForcedPos;
+        return particle;
+    }
+    public Particle CreateParticle()
     {
         GameObject newPrefab = GameObject.Instantiate(particlePrefab);
         Particle newParticle = newPrefab.GetComponent<Particle>();
@@ -17,18 +23,19 @@ public partial class SimController : MonoBehaviour
         newParticle.Initialize();
 
         //random position (uniform)
-        //newPrefab.transform.position = new Vector3(Random.Range(boundsMin.x * 0.9f, boundsMax.x * 0.9f), 0, Random.Range(boundsMin.z * 0.9f, boundsMax.z * 0.9f));
-        newPrefab.transform.position = new Vector3(0, 0, 0);
+        newPrefab.transform.position = new Vector3(Random.Range(boundsMin.x * 0.9f, boundsMax.x * 0.9f), 0, Random.Range(boundsMin.z * 0.9f, boundsMax.z * 0.9f));
+        //newPrefab.transform.position = new Vector3(0, 0, 0);
 
         //random starting velocity (gaussian and based off equipartition formula for thermal equilibrium)
         RandomGaussian.SetSigma(Mathf.Sqrt(1.38f * Mathf.Pow(10f, -23f) * Temp() / newParticle.Mass()));
-        newParticle.velocity = new Vector3(RandomGaussian.Generate(),0, RandomGaussian.Generate());
-        //newParticle.velocity = new Vector3(5, 0, 2);
+        newParticle.velocity = new Vector3(RandomGaussian.Generate(), 0, RandomGaussian.Generate());
+        //newParticle.velocity = new Vector3(200, 0, 100);
         newParticle.speed = newParticle.velocity.magnitude;
         //Debug.Log("Speed: " + newParticle.speed);
         //Debug.Log("Velocity: " + newParticle.velocity);
 
         //activate it
         newParticle.ToggleRunning();
+        return newParticle;
     }
 }
